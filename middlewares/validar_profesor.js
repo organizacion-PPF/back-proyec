f07bc210ebf502123c9b6cffeabcc0ec713c3b40
arirome <arielarome15.com@gmail.com>
2021-11-15 09:55:44 -0300

    check

--- middlewares/validar_profesor.js
+++ middlewares/validar_profesor.js
@@ -2,24 +2,24 @@ const { body, check } = require('express-validator');
 const {siExistemateria}= require('../middlewares/validar_materia')
 const validacionProfesor=
 [
-    body('descripcion', 'La descripcion ingresada no contiene un formato correcto')
+   check('descripcion', 'La descripcion ingresada no contiene un formato correcto')
     .isString()
     .not()
     .isEmpty()
     .isLength({ min: 15}),
 
-    body('nivel', 'el nivel ingresado no contiene un formato correcto')
+   check('nivel', 'el nivel ingresado no contiene un formato correcto')
     .isArray()
     .not()
     .isEmpty(),
 
-    body('materia', 'La materia ingresada no contiene un formato correcto')
+   check('materia', 'La materia ingresada no contiene un formato correcto')
     .isArray()
     .not()
     .isEmpty()
     .custom(siExistemateria),
 
-    body('horarios', 'el horario ingresado no contiene un formato correcto')
+   check('horarios', 'el horario ingresado no contiene un formato correcto')
     .isArray()
     .not()
     .isEmpty()
